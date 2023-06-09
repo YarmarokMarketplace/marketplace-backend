b62713479ffdf4d40974ddafbe70e34bc8f7a41d
Olena Stasiuk <estasjuk@gmail.com>
2023-06-09 19:55:39 +0300

    Environment setup

---
+++ server.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+const app = require("./app");
+
+const PORT = process.env.PORT || 8081;
+
+const start = async () => {
+  try {
+    await mongoose.connect(process.env.DB_HOST);
+    console.log("Database connection successful");
+    app.listen(PORT, () => console.log(`server started on port: ${PORT}`));
+  } catch (e) {
+    console.log(e);
+  }
+};
+start();
