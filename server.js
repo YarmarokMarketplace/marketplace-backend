d61c2afc269b0ba7b0491fa75ba253765e74d04b
Pavlo Zaloznyi <pzaloznyi@gmail.com>
2023-06-16 21:04:33 +0300

    fix ci/cd

--- server.js
+++ server.js
@@ -6,11 +6,15 @@ const PORT = process.env.PORT || 8081;
 
 const start = async () => {
   try {
-    await mongoose.connect(process.env.DB_HOST);
+    await client.connect();
+    await client.db('admin').command({ ping: 1 });
     console.log("Database connection successful");
     app.listen(PORT, () => console.log(`server started on port: ${PORT}`));
   } catch (e) {
     console.log(e);
+  } finally {
+    await client.close();
   }
 };
-start();
+
+start().catch(console.dir);
