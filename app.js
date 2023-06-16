09310a027ae4cc6afc48ee248f31800a9249e112
Pavlo Zaloznyi <pzaloznyi@gmail.com>
2023-06-16 15:56:42 +0300

    First content to render

--- app.js
+++ app.js
@@ -4,14 +4,35 @@ const cors = require("cors");
 require("dotenv").config();
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./swagger.json");
+const path = require('path');
 
 // const authRouter = require("./routes/api/auth/auth-routes");
 // const noticeRouter = require("./routes/api/notices/notices-routes");
 // const userRouter = require("./routes/api/user/user-routes");
 
 const app = express();
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res, next) => {
+  res.render('index', {
+      heading: 'YarmarOK',
+      text: 'Some text',
+      time: (new Date().toUTCString())
+  })
+})
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+
+app.get('/', (req, res) => {
+  res.render('index', {title: 'Hey', message: 'Hello World!'});
+});
 
 app.use(logger(formatsLogger));
 
