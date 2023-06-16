f28353feb9fb96923cd4015c6faceb56e5a6835a
Pavlo Zaloznyi <pzaloznyi@gmail.com>
2023-06-16 12:14:09 +0300

    Deployment setup

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const app = require("./app");
 
 const PORT = process.env.PORT || 8081;
 
-const start = async () => {
+const start = () => {
   try {
-    await mongoose.connect(process.env.DB_HOST);
+    // await mongoose.connect(process.env.DB_HOST);
     console.log("Database connection successful");
     app.listen(PORT, () => console.log(`server started on port: ${PORT}`));
   } catch (e) {
