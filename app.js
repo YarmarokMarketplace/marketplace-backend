eefe91d5543f9fee061ad3c287331a56f1a0ce0d
Pavlo Zaloznyi <pzaloznyi@gmail.com>
2023-06-16 21:04:33 +0300

    fix ci/cd

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
+require("dotenv").config();
+
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
-require("dotenv").config();
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./swagger.json");
+const path = require('path');
 
 // const authRouter = require("./routes/api/auth/auth-routes");
 // const noticeRouter = require("./routes/api/notices/notices-routes");
 // const userRouter = require("./routes/api/user/user-routes");
 
 const app = express();
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
+app.use('/', router);
 app.use(logger(formatsLogger));
 
 app.use(
